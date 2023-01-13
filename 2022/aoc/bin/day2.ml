f378a4a3fb27fe57791f23938b5e5dbc0af40324
Jonathan Schear <jonathanschear@gmail.com>
2023-01-13 14:59:06 -0500

    Split days into separate modules

--- 2022/aoc/bin/day2.ml
+++ 2022/aoc/bin/day2.ml
@@ -1,29 +1,6 @@
 open! Core
 
-let rec list_max l =
-  match l with [] -> Int.min_value | x :: xs -> max x (list_max xs)
-
-let elves =
-  In_channel.read_all "input/day1.txt"
-  |> Str.split (Str.regexp "\n\n")
-  |> List.map ~f:(Str.split (Str.regexp "\n"))
-  |> List.map ~f:(List.map ~f:int_of_string)
-
-let sums = List.map elves ~f:(List.fold ~init:0 ~f:( + ))
-
-let () =
-  let max = list_max sums in
-  Printf.printf "Part 1: %d\n" max
-
-let () =
-  let sorted = List.sort sums ~compare |> List.rev in
-  let sum_three_max =
-    match sorted with a :: b :: c :: _ -> a + b + c | _ -> 0
-  in
-  Printf.printf "Part 2: %d\n" sum_three_max
-
-(* Day 2 TODO: figure out how to move this into a different module :) *)
-(* Also TODO: figure out why Not_found is deprecated in Jane Street's stdlib *)
+(* TODO: figure out why Not_found is deprecated in Jane Street's stdlib *)
 let input = In_channel.read_all "input/day2.txt"
 let rounds = Str.split (Str.regexp "\n") input
 
